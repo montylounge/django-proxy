360a14b6a11958ab071e9e42d712ab13759868ee
kfricovsky@kevinmbp.local <kfricovsky@kevinmbp.local>
2009-10-08 22:29:51 -0400

    added try/catch to handle DoesNotExist on proxy models

--- django_proxy/signals.py
+++ django_proxy/signals.py
@@ -6,7 +6,7 @@ def proxy_save(sender, **kwargs):
     ''' Handles the save/update of a Proxy instance. '''
     instance = kwargs['instance']
     created = kwargs['created']
-    
+
     cls = instance.ProxyMeta
     model_str = getattr(cls, 'model_str', 'django_proxy.proxy')
     model = get_model(*model_str.split('.'))
@@ -14,11 +14,11 @@ def proxy_save(sender, **kwargs):
     #is this is a new instance?
     if created:
         obj = model()
-        
+
         #does this piece of content care about active state awareness?
         if hasattr(cls,'active'):
             try:
-                active_field = getattr(cls, 'active')           
+                active_field = getattr(cls, 'active')
                 objfield = active_field.keys()[0]
                 active_status = active_field.values()[0]
                 actual_status = getattr(instance, objfield, None)
@@ -27,16 +27,16 @@ def proxy_save(sender, **kwargs):
             except Exception:
                 #TODO: cleaner exception handling/messaging
                 pass
-        
+
         obj.content_object = instance
         obj.title = getattr(instance, cls.title, None)
         obj.description = getattr(instance, cls.description, None)
-        
+
         #pub_date isn't required so confirm
         pub_date = getattr(cls, 'pub_date', None)
         if pub_date:
             obj.pub_date = getattr(instance, cls.pub_date)
-        
+
         #tags aren't required so confirm
         tags = getattr(cls, 'tags', None)
         if tags:
@@ -54,7 +54,7 @@ def proxy_save(sender, **kwargs):
         #does this piece of content care about active state awareness?
         if obj.id != None and hasattr(cls,'active'):
             try:
-                active_field = getattr(cls, 'active')         
+                active_field = getattr(cls, 'active')
                 objfield = active_field.keys()[0]
                 active_status = active_field.values()[0]
                 actual_status = getattr(instance, objfield, None)
@@ -65,14 +65,14 @@ def proxy_save(sender, **kwargs):
                 #TODO: cleaner exception handling/messaging
                 pass
 
-        obj.title = getattr(instance, cls.title, None)            
+        obj.title = getattr(instance, cls.title, None)
         obj.description = getattr(instance, cls.description, None)
 
         #proxy pub_date isn't required so confirm
         pub_date = getattr(cls, 'pub_date', None)
         if pub_date:
             obj.pub_date = getattr(instance, cls.pub_date)
-        
+
         #proxy tag isn't require so confirm
         tags = getattr(cls, 'tags', None)
         if tags:
@@ -81,15 +81,19 @@ def proxy_save(sender, **kwargs):
 
 def proxy_delete(sender, **kwargs):
     '''Responsible for handling the deletion of any child/associated Proxy records.
-    
+
     Coupled to associated object's post_delete signal.
-    
+
     '''
     instance = kwargs['instance']
     ctype = ContentType.objects.get_for_model(instance)
 
     cls = instance.ProxyMeta
     model_str = getattr(cls, 'model_str', 'django_proxy.proxy')
-    model = get_model(*model_str.split('.'))    
-    obj =  model._default_manager.get(object_id=instance.id, content_type=ctype)
-    obj.delete()
+    model = get_model(*model_str.split('.'))
+    try:
+        obj =  model._default_manager.get(object_id=instance.id, content_type=ctype)
+        obj.delete()
+    except DoesNotExist:
+        pass
+
